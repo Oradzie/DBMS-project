c2b6d06450333c51f6437883dccd92d3e6d6ac1e
Orazio Spina <oraziospina@icloud.com>
2024-07-14 17:56:03 +0200

    Testing sql export

--- app/src/main/java/azienda/utilities/prova.sql
+++ app/src/main/java/azienda/utilities/prova.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `azienda` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `azienda`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: azienda
@@ -867,4 +869,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-14 17:50:39
+-- Dump completed on 2024-07-14 17:55:44

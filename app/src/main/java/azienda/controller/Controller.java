52925b96e3dc3ba00d193f5901ef1c32ecc2ab47
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-07-14 16:52:33 +0200

    Fixed errors

--- app/src/main/java/azienda/controller/Controller.java
+++ app/src/main/java/azienda/controller/Controller.java
@@ -1,16 +1,16 @@
 package azienda.controller;
 
-import db_lab.data.DAOException;
-import db_lab.data.ProductPreview;
-import db_lab.model.Model;
+import azienda.commons.DAOException;
+import azienda.model.Model;
 import java.util.Objects;
+import azienda.view.MainView;
 
 public final class Controller {
 
     private final Model model;
-    private final View view;
+    private final MainView view;
 
-    public Controller(Model model, View view) {
+    public Controller(Model model, MainView view) {
         Objects.requireNonNull(model, "Controller created with null model");
         Objects.requireNonNull(view, "Controller created with null view");
         this.view = view;
--- app/src/main/java/azienda/model/Model.java
+++ app/src/main/java/azienda/model/Model.java
@@ -1,8 +1,11 @@
 package azienda.model;
 
-public final class Model {
-    // Create a model that connects to a database using the given connection.
-    public static Model fromConnection(Connection connection) {
+import java.sql.Connection;
+
+public interface Model {
+     // Create a model that connects to a database using the given connection.
+     public static Model fromConnection(Connection connection) {
         return new DBModel(connection);
     }
+    
 }

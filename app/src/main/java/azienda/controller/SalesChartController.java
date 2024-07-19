444be91d24467a489fcdcd7feb0d597aabe37728
Orazio Spina <oraziospina@icloud.com>
2024-07-19 12:13:09 +0200

    Added chart data

--- app/src/main/java/azienda/controller/SalesChartController.java
+++ app/src/main/java/azienda/controller/SalesChartController.java
@@ -26,19 +26,9 @@ public class SalesChartController {
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName("Monthly Sales");
 
-        // Example sales data
-        series.getData().add(new XYChart.Data<>("Gennaio", 200));
-        series.getData().add(new XYChart.Data<>("Febbraio", 150));
-        series.getData().add(new XYChart.Data<>("Marzo", 300));
-        series.getData().add(new XYChart.Data<>("Aprile", 250));
-        series.getData().add(new XYChart.Data<>("Maggio", 400));
-        series.getData().add(new XYChart.Data<>("Giugno", 350));
-        series.getData().add(new XYChart.Data<>("Luglio", 500));
-        series.getData().add(new XYChart.Data<>("Agosto", 450));
-        series.getData().add(new XYChart.Data<>("Settembre", 600));
-        series.getData().add(new XYChart.Data<>("Ottobre", 550));
-        series.getData().add(new XYChart.Data<>("Novembre", 700));
-        series.getData().add(new XYChart.Data<>("Dicembre", 650));
+        for (Pair<String, Integer> pair : this.salesData) {
+            series.getData().add(new XYChart.Data<>(pair.first(), pair.second()));
+        }
 
         // Add series to chart
         salesChart.getData().add(series);

37bf72b0e423b0ce4e5baaffcf8e73aa3506ab69
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-07-19 16:13:21 +0200

    Modified class MagazzinieriStatisticheController.java

--- app/src/main/java/azienda/controller/MagazzinieriStatisticheController.java
+++ app/src/main/java/azienda/controller/MagazzinieriStatisticheController.java
@@ -1,12 +1,5 @@
 package azienda.controller;
 
-import azienda.App;
-import azienda.commons.Pair;
-import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
-import javafx.scene.layout.Pane;
-
 public class MagazzinieriStatisticheController {
     
 }
